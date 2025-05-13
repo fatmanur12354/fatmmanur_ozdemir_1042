72c299066030edef11b8026050ade414643690bb
Taner ERKAN <Tanererkan06@users.noreply.github.com>
2025-05-13 23:46:25 +0300

    kayit sayfasi
    fix

--- app/src/main/java/com/example/projeodevi/MainActivity.java
+++ app/src/main/java/com/example/projeodevi/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.projeodevi;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +13,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button bttn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +24,16 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button bttn = (Button) findViewById(R.id.kayit);
+        bttn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(MainActivity.this,register.class);
+                        startActivity(i);
+                    }
+                }
+        );
     }
 }
