cb79aa426527bf3f6a2fe4569c094cc96118d0de
Taner ERKAN <Tanererkan06@users.noreply.github.com>
2025-05-15 21:46:11 +0300

    fix
    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,8 +43,10 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
--- app/src/main/java/com/example/projeodevi/registers.java
+++ app/src/main/java/com/example/projeodevi/registers.java
@@ -1,5 +1,6 @@
 package com.example.projeodevi;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -7,6 +8,8 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -14,11 +17,14 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.projeodevi.databinding.RegistersBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class registers extends AppCompatActivity {
-
+    FirebaseAuth mAuth;
     private AppBarConfiguration appBarConfiguration;
     private RegistersBinding binding;
+    private Button Kaydet;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +34,22 @@ public class registers extends AppCompatActivity {
         setContentView(binding.getRoot());
 
 
+        Kaydet = findViewById(R.id.button3);
+        mAuth = FirebaseAuth.getInstance();
+        Kaydet.setOnClickListener(new View.OnClickListener() {
+                                     @Override
+                                     public void onClick(View v) {
+                                         registerfunc();
+
+                                     }
+                                 }
+        );
 
 
+    }
 
+    private void registerfunc() {
+        Toast.makeText(this, "test", Toast.LENGTH_SHORT).show();
     }
 
 
