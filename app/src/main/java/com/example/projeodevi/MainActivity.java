eeee75cbd25c03ba966abd6232499a9ff09c9b18
Taner ERKAN <Tanererkan06@users.noreply.github.com>
2025-05-16 03:57:56 +0300

    giriş
    fix

--- app/src/main/java/com/example/projeodevi/MainActivity.java
+++ app/src/main/java/com/example/projeodevi/MainActivity.java
@@ -2,18 +2,31 @@ package com.example.projeodevi;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
     private Button kayit,giris;
+    TextInputEditText mail;
+    TextInputEditText sifre;
+    FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,8 +41,7 @@ public class MainActivity extends AppCompatActivity {
         giris.setOnClickListener(new View.OnClickListener() {
                                      @Override
                                      public void onClick(View v) {
-                                         Intent i = new Intent(MainActivity.this, result.class);
-                                         startActivity(i);
+                                       loginUser();
 
                                      }
                                  }
@@ -44,5 +56,31 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
         );
+    }
+
+    private void loginUser(){
+        String email = mail.getText().toString();
+        String password = sifre.getText().toString();
+
+        if (TextUtils.isEmpty(email)){
+            mail.setError("Email cannot be empty");
+            mail.requestFocus();
+        }else if (TextUtils.isEmpty(password)){
+            sifre.setError("Password cannot be empty");
+            sifre.requestFocus();
+        }else{
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                       Toast.makeText(MainActivity.this, "Başarı ile giriş yapıldı", Toast.LENGTH_SHORT).show();
+                        Intent i = new Intent(MainActivity.this, result.class);
+                        startActivity(i);
+                    }else{
+                        Toast.makeText(MainActivity.this, "Log in Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
     }
 }
